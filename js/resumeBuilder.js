ae04609976c377c9b15dc945922ec82821faacc1
Laurie <laurie.walroth@gmail.com>
2014-10-10 18:12:24 -0700

    hwk2

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -10,25 +10,62 @@ var bio = {
 	"role" : "Software Engineer",
 	"Contact Info" : "laurie.walroth@gmail.com",
 	"Welcome Message" : "Welcome to my page",
-	"skills" : ["Python", "Java", "Web Design"]
-};
-
-$("#main").append(bio.name);
-
-var work = {};
-work.position = "Software Engineer";
-work.employer = "Google";
-work.years = 3;
-work.location = "SunnyVale";
-
-var education = {};
-education["name"] = "UofT";
-education["years"] = 4;
-education["city"] = "Toronto";
+	"skills" : [
+	  "Python", "Java", "Web Design"
+	],
+  
+    "welcomeMessage": "Welcome to my Resume",
+}
 
 
-$("#main").append(work["position"]);
-$("#main").append(education.name);
+
+var work = {
+	"jobs": [
+	  {
+	  	"employer": "Google",
+	  	"title" : "Software Engineer",
+	  	"dates" : "August 2014 - Present",
+	  	"description" : "working"
+	  }
+	]
+}
+
+
+
+var education = {
+	"schools": [
+	  {
+		"name": "UofT",
+		"city": "Toronto",
+		"major": "english",
+		"minor": "sociology",
+		"graduation": 2004
+		
+	  }
+	],
+
+    "onlineCourse": [
+      {
+      	"title": "Javascript",
+      	"school": "Udacity",
+      	"date": 2014
+      }
+    ]
+}
+
+var projects = {
+	"projects": [
+	  {
+	  	"title": "Sample Project 1",
+	  	"dates": "2014"
+	  }
+	]
+}
+
+$("#main").append(bio.role);
+$("#main").append(work.jobs[0]);
+$("#main").append(education[schools.name]);
+$("#main").append(projects.projects);
 
 
 
